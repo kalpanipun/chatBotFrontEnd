5868c03ac1f1319600526a0491331d57fe3a7978
kalpa nipun <kalpanipun7@gmail.com>
2024-03-21 22:34:18 +0530

    function file update

---
+++ model.py
@@ -0,0 +1,8 @@
+from langchain_openai import ChatOpenAI
+import streamlit as st
+import openai
+import matplotlib.pyplot as plt
+
+
+openai.api_key = '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'
+
