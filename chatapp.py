0f9823fd82bf90ad3aa2905ecbb2d4fd865518f8
kalpa nipun <kalpanipun7@gmail.com>
2024-03-23 18:11:14 +0530

    final update befor deploy

--- chatapp.py
+++ chatapp.py
@@ -1,21 +1,19 @@
 import streamlit as st
-#from streamlit_dynamic_filters import DynamicFilters
-# from st_aggrid import AgGrid
-# from st_aggrid.grid_options_builder import GridOptionsBuilder
-# from st_aggrid import GridUpdateMode, DataReturnMode
 import warnings
-# import plotly.express as px
-# import plotly.graph_objects as go
-# import requests
 import os
-# import ast
-# from io import BytesIO
-import openai
+import re
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 
+# Reading Methods
 from functions import generate_data
-from functions import create_bar_graph
+from read_data import load_index
+from read_data import load_pdf
 
+# Data path
+file_path = 'financial_statements_csv.csv'
 
+stock_path = 'stock_mkt_data4.csv'
 
 # hide future warnings (caused by st_aggrid)
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -23,63 +21,184 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 # set page layout and define basic variables
 st.set_page_config(layout="wide", page_icon='⚡', page_title="Instant Insight")
 path = os.path.dirname(__file__)
-#today = date.today()
+# today = date.today()
 
 # create sidebar filters
-st.sidebar.write('**Select the sector if you want** 👇')
+############################ All the markdowns #############################################
 
-with st.sidebar:
-    options = ['Banking', 'Finance', 'Insurance']
+################## Logo ###################################################
+st.markdown(
+    """
+    <style>
+    .logo-container {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 20px;
+    }
+    .logo-img {
+        max-width: 100px; /* Adjust the max-width as needed */
+        height: auto;
+    }
+    </style>
+    """
+    , unsafe_allow_html=True
+)
+# Logo container
+st.markdown('<div class="logo-container">', unsafe_allow_html=True)
+# Logo image
+st.markdown('<img src="https://i.ibb.co/Dz3rmgq/logo-copy.png" class="logo-img">', unsafe_allow_html=True)
+# Close the logo container
+st.markdown('</div>', unsafe_allow_html=True)
+#########################################################################################
 
-    # Create a dropdown menu using st.selectbox
-    selected_option = st.selectbox('Choose from available sectors', options)
 
-    # Display the selected option
-    st.write('You selected:', selected_option)
-    st.markdown("Developed by OCTOBOTS team")
-##############################################################################################################
+##################### button,Text and background colour ########################################
+st.markdown(
+    """
+    <style>
+    .stButton>button {
+        background-color: #007bff; /* Blue color for buttons */
+        color: #ffffff; /* White text color for buttons */
+    }
+    .stTextInput>div>div>input {
+        background-color: #ffffff; /* White color for text input boxes */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# background colour
 
-st.title('Your Financial Advisor !')
+st.markdown(
+    """
+    <style>
+    body {
+        background: linear-gradient(to right, #FFD700, #FFA500); /* Gradient background colors */
+        font-family: Arial, sans-serif; /* Set font family */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Text Box
+st.markdown(
+    """
+    <style>
+    .stTextInput>div>div>input {
+        border: 1px solid #0e2f44; /* Add a 1px solid border with gray color */
+        padding: 8px; /* Add padding to the input elements */
+        border-radius: 4px; /* Add border radius for rounded corners */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+###############################################################################
 
-with st.expander('What is this app about?'):
+st.sidebar.markdown(
+    """
+    <style>
+    .sidebar .sidebar-content {
+        background-color: #00FF00 !important; /* Green color */
+    }
+    </style>
+    <div style="position: fixed; bottom: 10px; left: 10px;">
+        <p style="font-size: 12px;"> Developed by OCTBOTS team </p>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+#############################################################################################
+
+st.sidebar.write('**How to use this application??** ')
+st.sidebar.write('1. Provide your investment objective and risk appetite to get the best fitting stocks for your portfolio!?? ')
+st.sidebar.write('2. Visualize the stocks in any fashion to make an informed decision.')
+
+with st.expander('Future of this app'):
     st.write('''
-    This app is designed to generate an instant company research.\n
-    In a matter of few clicks, a user gets details about ...
-
-    Use Case Example:\n
-    
-
-    Tech Stack:\n
-    
+    To update it to match the CSE 
     ''')
 
-user_query = st.text_input('Enter your query here:', '')
+##############################################################################################################
+
+index = load_index(stock_path)
+chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(), chain_type="stuff",
+                                    retriever=index.vectorstore.as_retriever(), input_key="question")
+
+st.title('Your Financial Advisor! 💰')
+
+user_query = st.text_input('Ask anything about the stocks : ', '')
+template = (f"{user_query},If there are any graphs to be plot give python code for that")
+
+
 
 # Create a placeholder for the response
 response_placeholder = st.empty()
-
 send_button = st.button('Send')
-
-def generate_response(query):
-    return query
 
 # Create a button to submit the query and trigger the conversation flow
 if send_button:
     # Generate a response based on the user's query
 
-    data = generate_data(user_query)
-    st.write(data)
 
-    #response = generate_response(user_query)
-    # Display the response
-    create_bar_graph(data)
+    index = load_index(stock_path)
 
-    #response_placeholder.text_area('ChatGPT:', value=response, height=200)
+    # chain = RetrievalQA.from_chain_type(llm=ChatOpenAI(), chain_type="stuff",
+    #                                     retriever=index.vectorstore.as_retriever(), input_key="question")
 
-    #user_query = st.text_input('You:', '')
+    # response = chain({"question": template})
 
+    result = index.query(template, llm=ChatOpenAI())
 
-# Display the user's query
-#st.write('You entered:', user_query)
+    # Remove python code from the output
+
+    # pattern = r"(?s)(?<!```python\n)(.*?)(?!```)"
+    # extracted_text = re.findall(pattern, response['result'])
+    # st.write(extracted_text)
+
+    code_match = re.search(r'```python\n(.*?)\n```', result, re.DOTALL)
+    if code_match:
+        generated_code = code_match.group(1)
+
+        try:
+            exec(generated_code)
+
+            st.pyplot()
+
+        except Exception as e:
+            st.error(f"Error executing code: {e}")
+
+# personal data
+
+personal_query = st.text_input('Tell us about your personal financial goal to generate the best stocks that fit you: ', '')
+response_placeholder_p = st.empty()
+send_button_p = st.button('Go')
+
+static_query_template_p = (f"I have given a historical dataset on stock market prices for 7 assets. Act as a helpful "
+                           f"financial advisor."
+                           f"Map the appropriate stocks based on my personal specifications. "
+                           f"In order to map, take into account the stock return, "
+                           f"coefficient of variation to make a decision. {personal_query} "
+                           f"Which stocks are appropriate for me?" 
+                           f"Provide a short answer,If there are any graphs to be plot give python code for that")
+
+if send_button_p:
+
+    result_p = index.query(static_query_template_p, llm=ChatOpenAI())
+    st.write(result_p)
+
+    code_match = re.search(r'```python\n(.*?)\n```', result_p, re.DOTALL)
+    if code_match:
+        generated_code = code_match.group(1)
+
+        try:
+            exec(generated_code)
+
+            st.pyplot()
+
+        except Exception as e:
+            st.error(f"Error executing code: {e}")
+
 
 
--- functions.py
+++ functions.py
@@ -1,40 +1,19 @@
-from langchain_openai import ChatOpenAI
-import streamlit as st
 from openai import OpenAI
-import matplotlib.pyplot as plt
 import os
-import re
 
-os.environ["OPENAI_API_KEY"] = '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'
+#os.environ["OPENAI_API_KEY"] = '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'
 
+os.environ["OPENAI_API_KEY"] = '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'
 client = OpenAI()
 OpenAI.api_key = os.getenv('OPENAI_API_KEY')
 
-def generate_data(query):
 
+def generate_data(query):
     completion = client.chat.completions.create(
         model='gpt-3.5-turbo',
         messages=[  # Change the prompt parameter to messages parameter
-            {'role': 'user', 'content': query} ],
+            {'role': 'user', 'content': query}],
         temperature=0
     )
-
     data = completion.choices[0].message.content
     return data
-
-
-def extract_data(api_output):
-    # Regular expression pattern to match the data lines
-    pattern = r'(\d{4}): \$([\d,]+)'
-    matches = re.findall(pattern, api_output)
-    years = [int(match[0]) for match in matches]
-    revenues = [int(match[1].replace(',', '')) for match in matches]
-    return years, revenues
-
-def create_bar_graph(data):
-    years, revenues = extract_data(data)
-    plt.bar(years, revenues)
-    plt.xlabel('Year')
-    plt.ylabel('Sales Revenue ($)')
-    plt.title('Sales Revenue Over the Past 5 Years')
-    st.pyplot()
